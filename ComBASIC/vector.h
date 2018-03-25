fa2674cdc4b365d094a37f4154c5da087ecff848
tearth <Tearth@users.noreply.github.com>
2018-03-26 00:36:19 +0200

    Remove vector_dump.

--- ComBASIC/vector.h
+++ ComBASIC/vector.h
@@ -17,4 +17,3 @@ void* vector_get(vector* vector, int index);
 void vector_set(vector* vector, int index, void* data);
 void vector_remove(vector* vector, int index);
 void vector_free(vector* vector);
-void vector_dump(vector* vector);
