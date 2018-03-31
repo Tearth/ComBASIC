f087f4666ca042bd70b49c1e7491964661f07415
tearth <Tearth@users.noreply.github.com>
2018-03-31 20:48:40 +0200

    Add function for rpn tree build.

--- ComBASIC/parser-expression.c
+++ ComBASIC/parser-expression.c
@@ -2,14 +2,15 @@
 
 ast_node* parser_expression_build(vector* tokens, ast_node* keyword, int* index, vector* symbol_table)
 {
-	ast_node* expression_node = (ast_node*)malloc(sizeof(ast_node));
-	astnode_init(expression_node);
-	expression_node->type = N_EXPRESSION;
+	ast_node* expression_root = (ast_node*)malloc(sizeof(ast_node));
+	astnode_init(expression_root);
+	expression_root->type = N_EXPRESSION;
 
 	vector* rpn = parser_expression_buildrpn(tokens, keyword, index, symbol_table);
 	vector* rpn_nodes = parser_expression_buildrpnnodes(rpn, symbol_table);
+	parser_expression_buildrpntree(rpn_nodes, expression_root);
 
-	return expression_node;
+	return expression_root;
 }
 
 vector* parser_expression_buildrpn(vector* tokens, ast_node* keyword, int* index, vector* symbol_table)
@@ -120,6 +121,8 @@ vector* parser_expression_buildrpnnodes(vector* rpn, vector* symbol_table)
 			case T_NUMBER:
 			{
 				node->type = N_NUMBER;
+				string_append_s(&node->value, current_token->value.data);
+
 				break;
 			}
 
@@ -155,6 +158,36 @@ vector* parser_expression_buildrpnnodes(vector* rpn, vector* symbol_table)
 	}
 
 	return rpn_nodes;
+}
+
+void parser_expression_buildrpntree(vector* rpn_nodes, ast_node* expression_root)
+{
+	vector* stack = (vector*)malloc(sizeof(vector));
+	vector_init(stack);
+
+	for (int i = 0; i < rpn_nodes->count; i++)
+	{
+		ast_node* current_node = rpn_nodes->data[i];
+		if (current_node->type == N_VARIABLE || current_node->type == N_NUMBER)
+		{
+			vector_add(stack, current_node);
+		}
+		else
+		{
+			ast_node* first = stack->data[stack->count - 1];
+			ast_node* second = stack->data[stack->count - 2];
+
+			vector_add(&current_node->children, first);
+			vector_add(&current_node->children, second);
+
+			vector_remove(stack, stack->count - 1);
+			vector_remove(stack, stack->count - 1);
+
+			vector_add(stack, current_node);
+		}
+	}
+
+	vector_add(&expression_root->children, stack->data[0]);
 }
 
 bool parser_expression_istokenvalid(token* token)
--- ComBASIC/parser-expression.h
+++ ComBASIC/parser-expression.h
@@ -6,6 +6,7 @@
 ast_node* parser_expression_build(vector* tokens, ast_node* keyword, int* index, vector* symbol_table);
 vector* parser_expression_buildrpn(vector* tokens, ast_node* keyword, int* index, vector* symbol_table);
 vector* parser_expression_buildrpnnodes(vector* rpn, vector* symbol_table);
+void parser_expression_buildrpntree(vector* rpn_nodes, ast_node* expression_root);
 
 bool parser_expression_istokenvalid(token* token);
 bool parser_expression_isparenthesis(token* token);
