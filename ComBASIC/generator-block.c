52775ccbb36af034e2867c5193b3829cad3255f3
tearth <Tearth@users.noreply.github.com>
2018-04-06 14:10:10 +0200

    Add GOTO generator.

--- ComBASIC/generator-block.c
+++ ComBASIC/generator-block.c
@@ -16,6 +16,7 @@ void generator_block_build(string* code, ast_node* root, vector* symbol_table)
 			case N_LET:		{ generator_let_build(code, keyword_node, symbol_table); break; }
 			case N_END:		{ generator_end_build(code, keyword_node, symbol_table); break; }
 			case N_IF:		{ generator_if_build(code, keyword_node, symbol_table); break; }
+			case N_GOTO:	{ generator_goto_build(code, keyword_node, symbol_table); break; }
 			default:
 			{
 				printf("ERROR: Invalid keyword.\n");
---
+++ ComBASIC/generator-goto.c
@@ -0,0 +1,10 @@
+#include "generator-goto.h"
+
+void generator_goto_build(string* code, ast_node* root, vector* symbol_table)
+{
+	char buffer[128];
+	ast_node* line_number = root->children.data[0];
+
+	sprintf_s(buffer, 128, "\tjmp \t_%s", line_number->value.data);
+	string_append_s(code, buffer);
+}
