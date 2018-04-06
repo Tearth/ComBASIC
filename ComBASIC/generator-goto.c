b4530a83da33530640922c6a5fee6c2b430510bc
tearth <Tearth@users.noreply.github.com>
2018-04-06 14:55:53 +0200

    Fix invalid goto generator.

--- ComBASIC/generator-goto.c
+++ ComBASIC/generator-goto.c
@@ -5,6 +5,6 @@ void generator_goto_build(string* code, ast_node* root, vector* symbol_table)
 	char buffer[128];
 	ast_node* line_number = root->children.data[0];
 
-	sprintf_s(buffer, 128, "\tjmp \t_%s", line_number->value.data);
+	sprintf_s(buffer, 128, "\tjmp \t_%s\n", line_number->value.data);
 	string_append_s(code, buffer);
 }
