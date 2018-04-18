589857ccdcd1534bf52eb99354140801736d10b9
tearth <Tearth@users.noreply.github.com>
2018-04-18 12:12:31 +0200

    Add comments to file manager.

--- ComBASIC/file-manager.h
+++ ComBASIC/file-manager.h
@@ -2,5 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+* @brief Loads a content of the specified file and returns it as string array. If file doesn't exists, then error
+*		 message is printed and app is closed.
+* @param filename Path to the specified file.
+* @return The file content.
+*/
 const char* file_load(const char* filename);
+
+/**
+* @brief Saves a content to the specified file. Note that previous data will be erased.
+*		 If file doesn't exists, then error message is printed and app is closed.
+* @param filename Path to the specified file.
+* @param content The content to save.
+* @param length The content length.
+*/
 void file_save(const char* filename, const char* content, int length);
